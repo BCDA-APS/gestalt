1e7c1464b5ab43d404fd3c2cf57d42d6391a6286
keenanlang <klang@aps.anl.gov>
2021-08-09 20:58:59 -0500

    Switched spreadsheet data to CSV

--- Spreadsheet.py
+++ Spreadsheet.py
@@ -1,29 +1,14 @@
-import openpyxl
+import csv
+
 
 def rows(filename):
-	ws = openpyxl.load_workbook(filename, read_only=True).active
+	output = []
+	
+	with open(filename) as the_data:
+		reader = csv.DictReader(the_data, delimiter=',', quotechar='"', skipinitialspace=True)
 		
-	for row_index in range(2, ws.max_row + 1):
-		row = {}
+		for row in reader:
+			output.append(row)
+			
+	return output
 		
-		for col_index in range(1, ws.max_column + 1):
-			key = str(ws.cell(row=1, column=col_index).value)
-			val = str(ws.cell(row=row_index, column=col_index).value)
-		
-			row[key] = val
-
-		yield row
-
-def cols(filename):
-	ws = openpyxl.load_workbook(filename, read_only=True).active
-		
-	for col_index in range(2, ws.max_column + 1):
-		col = {}
-		
-		for row_index in range(1, ws.max_row + 1):
-			key = str(ws.cell(row=row_index, column=1).value)
-			val = str(ws.cell(row=row_index, column=col_index).value)
-		
-			col[key] = val
-
-		yield col
