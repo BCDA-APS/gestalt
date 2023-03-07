a3fcf762a3a1394d1a988e4442ecc050dcb46e9a
keenanlang <klang@aps.anl.gov>
2023-03-07 00:08:19 -0600

    add Form-level margins

--- Gestalt.py
+++ Gestalt.py
@@ -221,7 +221,7 @@ class QtWidget(GroupNode):
 class QtDisplay(QtWidget):
 	def __init__(self, layout=None):
 		super(QtDisplay, self).__init__("QWidget", name="centralwidget")
-		
+	
 		self.form = QtWidget("QWidget", name="Form", layout=layout)
 	
 		self.form.append(self, keep_original=True)
@@ -235,7 +235,14 @@ class QtDisplay(QtWidget):
 		
 		
 	def writeQt(self, filename):
-		self.form["geometry"] = self["geometry"]
+		margins = Rect(x=0, y=0, width=0, height=0)
+		margins = margins.merge(self.form.attrs.pop("margins", Rect(x=0, y=0, width=0, height=0)))
+		
+		self.form["geometry"]["width"]  = self["geometry"]["width"] + margins["x"] + margins["width"]
+		self.form["geometry"]["height"] = self["geometry"]["height"] + margins["y"] + margins["height"]
+		
+		self["geometry"]["x"] = margins["x"]
+		self["geometry"]["y"] = margins["y"]
 		
 		tree = TreeBuilder()
 		
