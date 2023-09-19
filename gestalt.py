00f2f7d6cae49d81db84fefc465588df8c061fc2
keenanlang <klang@aps.anl.gov>
2023-09-18 22:37:26 -0500

    Add generator ui as primary script

--- gestalt.py
+++ gestalt.py
@@ -1,22 +1,87 @@
-import yaml
-import csv
+#! /APSshare/anaconda3/x86_64/bin/python3
+
+import os
+import tempfile
+import traceback
+
+from templates import *
+from gestalt import *
 
 
-def rows(filename):
-	output = []
+from PyQt5 import uic
+from PyQt5 import QtGui
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QStringListModel
 	
-	with open(filename) as the_data:
-		reader = csv.DictReader(the_data, delimiter=',', quotechar='"', skipinitialspace=True)
+
+class UI(QMainWindow):
+	def __init__(self):
+		super(UI, self).__init__()
+		uic.loadUi(".data/Form.ui", self)
 		
-		for row in reader:
-			output.append(row)
+		self.setWindowTitle("GESTALT")
+		
+		self.TemplateSelect.currentIndexChanged.connect(self.template_selected)
+		self.TemplateSelect.addItems(registry.templates.keys())
+		
+		self.LoadButton.clicked.connect(self.load_data)
+		self.WriteButton.clicked.connect(self.write_data)
+		
+		self.show()
+
+		
+	def template_selected(self, selection):
+		module_selected = registry.templates[self.TemplateSelect.itemText(selection)]
+		
+		text = ""
+		
+		for item in module_selected["required_inputs"]:
+			text += item[0]
+			text += " - "
+			text += item[1]
+			text += "\n"
+
+		self.TemplateInfo.setText(text)
+		self.InputData.clear()
+		self.InputData.appendPlainText(module_selected["example"])
+		self.Status.setText("")
+		self.TemplateThumbnail.setPixmap(QtGui.QPixmap(module_selected["thumbnail"]))
+		
+		
+	def load_data(self):
+		input_file = QFileDialog.getOpenFileName(self, "Open Input Data", filter="*.yml")[0]
+		
+		if input_file == "":
+			return
 			
-	return output
+		with open(input_file) as read_data:
+			self.InputData.clear()
+			self.InputData.appendPlainText(read_data.read())
+			self.Status.setText(input_file + " loaded")
+		
+		
+	def write_data(self):
+		output_file = QFileDialog.getSaveFileName(self, "Save Generated File", "", filter="*.ui")[0]
+		
+		if output_file == "":
+			return
+		
+		module_selected = registry.templates[self.TemplateSelect.itemText(self.TemplateSelect.currentIndex())]
+		
+		current_stylesheet = module_selected["stylesheet"]
+		
+		try:
+			Gestalt.generateQtFile(stylesheet=current_stylesheet, datastr=self.InputData.toPlainText(), outputfile=output_file, searchpath="./templates")
+			self.Status.setText("File Generated")
+			
+		except Exception as e:
+			traceback.print_exc()
+			QMessageBox.warning(self, "Error Occured", str(e))
+		
 		
 
-def parseFile(filename):
-	with open(filename) as the_file:
-		return yaml.safe_load(the_file)
+app = QApplication([])
 
-def parseString(data):
-	return yaml.safe_load(data)
+window = UI()
+app.exec_()
