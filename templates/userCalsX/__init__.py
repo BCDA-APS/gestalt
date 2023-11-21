22c834351e28e04559298645f5ea76eea5ceffc1
keenanlang <klang@aps.anl.gov>
2023-11-21 00:15:08 -0600

    switch to iterate over value

--- templates/userCalsX/__init__.py
+++ templates/userCalsX/__init__.py
@@ -3,21 +3,7 @@ from .. import registry
 registry.add("userCalc breakout", path=__path__,
 	qt_stylesheet = "layout.yml",
 	required_inputs=[
-("INDICES", 
-"""List of numbers indicating each userCalc to link:
-	
-  N: index of userCalc
-""")], 
+("NUM_CALCS", "How many lines to generate")], 
 example=
-"""INDICES:
-    - N: 0
-    - N: 1
-    - N: 2
-    - N: 3
-    - N: 4
-    - N: 5
-    - N: 6
-    - N: 7
-    - N: 8
-    - N: 9
+"""NUM_CALCS: 10
 """)
