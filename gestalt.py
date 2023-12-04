361b04cdd23df0d69e76b54537466324dcff0803
keenanlang <klang@aps.anl.gov>
2023-12-03 20:13:06 -0600

    add command-line access to generation

--- gestalt.py
+++ gestalt.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 
 import os
+import sys
+import argparse
 import pathlib
 import tempfile
 import traceback
@@ -17,14 +19,14 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QStringListModel
 	
+curr_dir = str(pathlib.Path(__file__).resolve().parent.resolve())
+
 
 class UI(QMainWindow):
 	def __init__(self):
 		super(UI, self).__init__()
-		
-		self.curr_dir = str(pathlib.Path(__file__).resolve().parent.resolve())
-		
-		uic.loadUi(self.curr_dir + "/.data/Form.ui", self)
+				
+		uic.loadUi(curr_dir + "/.data/Form.ui", self)
 		
 		self.setWindowTitle("GESTALT")
 			
@@ -107,8 +109,8 @@ class UI(QMainWindow):
 		if output_file == "":
 			return
 		
-		try:			
-			includes_dirs = str.split(".:" + self.curr_dir + ":" + self.curr_dir + "/templates:" + module_selected["path"], ":")
+		try:
+			includes_dirs = str.split(".;" + curr_dir + ";" + curr_dir + "/templates;" + module_selected["path"], ";")
 							
 			styles = Stylesheet.parse(current_stylesheet, includes_dirs)
 			data = Datasheet.parseString(self.InputData.toPlainText())
@@ -124,10 +126,120 @@ class UI(QMainWindow):
 		except Exception as e:
 			traceback.print_exc()
 			QMessageBox.warning(self, "Error Occured", str(e))
-		
-		
 
-app = QApplication([])
+			
 
-window = UI()
-app.exec_()
+			
+parser = argparse.ArgumentParser(prog='gestalt', usage='%(prog)s [OPTIONS] [TEMPLATE]', formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument("template", nargs="?", metavar="TEMPLATE", type=str, help="The template file used in constructing the output")
+			
+parser.add_argument("-f", "-r", "--from", "--read", 
+	metavar="FORMAT", 
+	dest="in_format",  
+	action="store", 
+	help="""
+File parser that should be used for the input data file.
+
+Recognized values are ['yml', 'yaml'] (Default: 'yml')
+
+
+""", 
+	type=str,
+	default="yml", 
+	choices=["yml", "yaml"])
+		
+parser.add_argument("-t", "-w", "--to", "--write", 
+	metavar="FORMAT",
+	dest="out_format", 
+	action="store", 
+	help="""
+File type conversion that should be used for the output 
+UI file. 
+
+Recognized values are ['qt', 'css', 'ui', 'bob']
+(Default: 'qt')
+
+
+""", 
+	type=str,
+	default="qt", 
+	choices=["qt", "bob", "ui", "css"])
+			
+parser.add_argument("-o", "--output",
+	metavar="FILE",
+	dest="out_filename",
+	action="store",
+	help="""
+Output file name
+
+(Default: Generate from template file and output format)
+
+
+""",
+	type=str)
+
+parser.add_argument('-i', "--input",
+	metavar="FILE",
+	dest='in_filename',
+	action='store', 
+	help="""
+Input data file to apply to template file
+
+(Default: Template will be applied with no macros)
+
+
+""",
+	type=str)
+
+parser.add_argument("--include", 
+	metavar="FOLDER", 
+	dest="include_dirs", 
+	action="append", 
+help="""
+Folders to search for any files included by the template.
+Can be applied multiple times, one folder per argument.
+
+By default, the search path includes the current directory 
+and gestalt's template directory (for colors.yml).
+	
+	
+""",
+	type=str)
+	
+	
+	
+	
+#parser.add_argument('--out',     dest='output',   action='store', help="File to write out")
+
+args = parser.parse_args()
+
+			
+if __name__ == "__main__":
+	if (len(sys.argv) == 1):
+		app = QApplication([])
+		
+		window = UI()
+		app.exec_()
+		
+	elif args.template == None:
+		print("Template file required for command-line conversion")
+		
+	else:
+		include_dirs = [".", curr_dir + "/templates"]
+
+		if args.include_dirs:
+			include_dirs.extend(args.include_dirs)		
+
+		data = {}	
+		
+		if args.in_filename:
+			with open(args.in_filename) as the_data:
+				data = Datasheet.parseString(the_data.read())
+		
+		styles = Stylesheet.parse(args.template, include_dirs)
+		
+		if args.out_format == "qt" or args.out_format == "ui":
+			generateQtFile(styles, data, outputfile=args.out_filename)
+		elif args.out_format == "css" or args.out_format == "bob":
+			generateCSSFile(styles, data, outputfile=args.out_filename)
