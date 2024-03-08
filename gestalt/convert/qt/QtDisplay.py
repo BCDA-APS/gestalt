ab9e4e77461a758563fc2fff1866570c1f40d6e7
keenanlang <klang@aps.anl.gov>
2024-03-07 18:25:26 -0600

    Fix background bug

--- gestalt/convert/qt/QtDisplay.py
+++ gestalt/convert/qt/QtDisplay.py
@@ -12,10 +12,9 @@ class QtDisplay(QtWidget):
 		self.widg = QtWidget("QWidget", name="centralwidget", layout={})
 	
 		self.form.append(self.widg, keep_original=True)
-		self.widg.append(self, keep_original=True)
-	
-	
+		self.widg.append(self, keep_original=True)	
 		
+	
 	def setProperties(self, layout):
 		self.form.setProperties(layout)
 		
@@ -23,13 +22,10 @@ class QtDisplay(QtWidget):
 		self.form.setProperty(key, prop)
 		
 		
-	def writeQt(self, filename):
-		if "background" not in self.form.attrs:
-			self.form.attrs["background"] = Color("$BBBBBB") 
-		
+	def writeQt(self, filename):		
 		if "styleSheet" not in self.form.attrs:
-			bg_col = self.form.attrs["background"]
-			stylesheet_str = "QMainWindow#Form {background: rgba(" + str(bg_col["red"]) + "," + str(bg_col["green"]) + "," + str(bg_col["blue"]) + "," + str(bg_col["alpha"]) + ");}"
+			bg_col = Color(self.form.attrs.pop("background", "$BBBBBB"))
+			stylesheet_str = "QWidget#centralwidget {background: rgba(" + str(bg_col["red"]) + "," + str(bg_col["green"]) + "," + str(bg_col["blue"]) + "," + str(bg_col["alpha"]) + ");}"
 			stylesheet_str += "\nQPushButton::menu-indicator {image: url(none.png); width: 0}"
 			self.form.attrs["styleSheet"] = String(stylesheet_str)
 			
