dba3b3abfb65a99e99e83a89e28d49f3ae6f7603
keenanlang <klang@aps.anl.gov>
2024-03-18 23:19:03 -0500

    output independent

--- templates/userCalsX/__init__.py
+++ templates/userCalsX/__init__.py
@@ -2,6 +2,7 @@ from .. import registry
 
 registry.add("userCalc breakout", path=__path__,
 	qt_stylesheet = "layout.yml",
+	css_stylesheet = "layout.yml",
 	required_inputs=[
 ("NUM_CALCS", "How many lines to generate")], 
 example=
