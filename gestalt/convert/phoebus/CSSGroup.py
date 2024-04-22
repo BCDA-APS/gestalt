2a9709d64159f765ac227e56b88387262a1aa528
keenanlang <klang@aps.anl.gov>
2024-04-22 00:49:58 -0500

    checking against wrong val

--- gestalt/convert/phoebus/CSSGroup.py
+++ gestalt/convert/phoebus/CSSGroup.py
@@ -21,7 +21,7 @@ class CSSGroup(CSSWidget):
 		frame["line_width"] = Number(self.pop("border-width", 0))
 		frame["background"] = Color(self.pop("background", "$00000000"))
 		
-		if ( (int(frame["line_width"]) != 0 and frame["background"].val()["alpha"] != 0) or frame["background"].val()["alpha"] != 0):
+		if ( (int(frame["line_width"]) != 0 and frame["line"].val()["alpha"] != 0) or frame["background"].val()["alpha"] != 0):
 			self.children.insert(0, frame)
 			
 		super(CSSGroup, self).write(screen)
