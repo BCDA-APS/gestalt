d749c66cd23410ded836fd8570ee54d53da0f439
keenanlang <klang@aps.anl.gov>
2025-04-17 11:36:19 -0500

    Embed node can't inherit from group

--- gestalt/nodes/EmbedNode.py
+++ gestalt/nodes/EmbedNode.py
@@ -2,13 +2,30 @@ from gestalt.Type import *
 from gestalt.nodes.Node import Node
 from gestalt.nodes.GroupNode import GroupNode
 
-class EmbedNode(GroupNode):
+class EmbedNode(Node):
 	def __init__(self, name=None, layout={}, loc=None):
 		super(EmbedNode, self).__init__("Embed", name=name, layout=layout, loc=loc)
 	
-	def initApply(self, data):
-		self.children = []
+	def apply(self, generator):
+		data = yield
 		
-		for item in List(data.get(str(self["embedding"]), [])):
+		children = []
+		
+		for item in List(data.get(str(self["embedding"]), [])):			
 			if isinstance(item, Node):
-				self.append(item, keep_original=True)
+				children.append(item)
+				
+		for child in children:
+			applier = child.apply(generator)
+			
+			for increment in applier:
+				child_macros = copy.copy(data)
+				
+				try: 
+					widget = applier.send(child_macros)
+					
+					yield widget
+				except:
+					break
+					
+				data = yield
