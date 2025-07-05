8b363779a55856c4af63d3f849ab18548a53407b
keenanlang <klang@aps.anl.gov>
2025-07-04 21:34:23 -0500

    document

--- gestalt/nodes/CalcNode.py
+++ gestalt/nodes/CalcNode.py
@@ -1,3 +1,44 @@
+"""
+An invisible widget which can perform a calculation on a set of input PVs and provides the output as a PV local to the screen.
+
+The 
+
+
+* **Special Attributes**
+
+|    Name    |  Type  | Description|
+|------------|--------|------------|
+| pv         | String | The name used to reference the output values |
+| A          | String | Input PV, the value of which can be referenced as A in the calculation |
+| B          | String | Input PV, the value of which can be referenced as B in the calculation |
+| C          | String | Input PV, the value of which can be referenced as C in the calculation |
+| D          | String | Input PV, the value of which can be referenced as D in the calculation |
+| calc       | String | The calculation to perform, provided in python syntax |
+
+
+* **Example**
+
+```yaml
+
+# If the motor reports having GAIN_SUPPORT, show enable/disable LED
+EnableCalc: !Calc
+	A:    "{motor-pv}.MSTA"
+	calc: "(A&2048)==2048"
+	pv:   "{motor-pv}:TorqueDisplay"
+	
+EnableLED: !VStretch:Group
+	visibility: "{motor-pv}:TorqueDisplay"
+	children:
+		- !VCenter:Group
+			children:
+				- !Apply:OnOffLED
+					size: "{on-off-width}"
+					control-pv: "{motor-pv}.CNEN"
+
+```
+"""
+
+
 from gestalt.Type import *
 from gestalt.nodes.Node import Node
 
