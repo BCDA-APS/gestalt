ffcecbb7f7652426de9fa8273b45275485606806
keenanlang <klang@aps.anl.gov>
2026-04-24 15:38:08 -0500

    Duplicate padding

--- gestalt/convert/pydm/StyleWriters.py
+++ gestalt/convert/pydm/StyleWriters.py
@@ -4,26 +4,25 @@ def style_textentry(node):
 	stylesheet = """\
 {my_class}#{my_name}
 {{
-    padding: 0px 0px 0px 2px; 
-    border: 2px; 
+    border: 2px;
     padding: 0px;
-    border-style:inset; 
+    border-style:inset;
     border-color: rgba(43, 101, 114, 255) rgba(185, 242, 255, 255)  rgba(185, 242, 255, 255) rgba(43, 101, 114, 255);
 }}
 """.format(my_class=node.classname, my_name=node.name)
 	return stylesheet
-	
+
 def style_button(node):
 	col = Color(node["background"]).val()
-	
+
 	r = col["red"]
 	g = col["green"]
 	b = col["blue"]
-	
+
 	dr = int(r/255.0 * 170)
 	dg = int(g/255.0 * 170)
 	db = int(b/255.0 * 170)
-	
+
 	lr = int(min(1.2 * r, 255))
 	lg = int(min(1.2 * g, 255))
 	lb = int(min(1.2 * b, 255))
@@ -32,10 +31,10 @@ def style_button(node):
 {my_class}#{my_name}
 {{
 	border-color: rgba({dr}, {dg}, {db}, 255);
-	border-radius: 1px; 
-	padding: 0px; 
+	border-radius: 1px;
+	padding: 0px;
 	border-width: 3px;
-	border-style: outset; 
+	border-style: outset;
 	margin:0px
 }}
 {my_class}#{my_name}:pressed
@@ -47,7 +46,7 @@ def style_button(node):
 	background-color: rgba({lr}, {lg}, {lb}, 255);
 }}
 """.format(
-	my_class=node.classname, 
+	my_class=node.classname,
 	my_name=node.name,
 	dr = dr,
 	dg = dg,
@@ -55,17 +54,17 @@ def style_button(node):
 	lr = lr,
 	lg = lg,
 	lb = lb)
-	
+
 	node.toRemove.append("background")
 	return stylesheet
 
 def write_frameborder(node):
 	stylesheet = ""
-						
+
 	col = Color(node["border-color"]).val()
 	wid = Number(node["border-width"]).val()
 	style = String(node["border-style"]).val()
-		
+
 	if (int(wid) != 0) and (col["alpha"] != 0):
 		stylesheet += """
 {my_class}#{my_name}
@@ -90,14 +89,14 @@ def write_frameborder(node):
 
 	return stylesheet
 
-		
+
 def write_color(node):
 	background = node["background"]
 	text_color = node["foreground"]
-	
+
 	stylesheet = """\
 {my_class}#{my_name}
-{{    
+{{
     background: rgba({br},{bg},{bb},{ba});
     color: rgba({tr},{tg},{tb},{ta});
 }}
@@ -112,35 +111,35 @@ def write_color(node):
 	tg = int(text_color["green"]),
 	tb = int(text_color["blue"]),
 	ta = int(text_color["alpha"]))
-	
+
 	node.toRemove.append("background")
 	node.toRemove.append("foreground")
-	
+
 	return stylesheet
-		
+
 def write_font(node):
 	the_font = node["font"]
-		
+
 	style = the_font["style"].lower()
-		
+
 	if "regular" in style:
 		style = ""
-		
+
 	align = str(node["alignment"])
-		
+
 	frame_align = "center"
-		
+
 	if "AlignLeft" in align:
 		frame_align = "left"
 	elif "AlignRight" in align:
 		frame_align = "right"
-					
+
 	stylesheet = """\
 {my_class}#{my_name}
 {{
     font-family: {family};
     font: {style} {size}pt;
-	
+
     text-align: {lcr};
 }}
 """.format(
@@ -150,7 +149,7 @@ def write_font(node):
 	style  = style,
 	size   = the_font["size"],
 	lcr    = frame_align)
-	
+
 	node.toRemove.append("font")
 	node.toRemove.append("alignment")
 	return stylesheet
