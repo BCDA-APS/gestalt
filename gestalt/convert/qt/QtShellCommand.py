9812586b94dc2fb25dadc8ef9d322a9e38fb5646
keenanlang <klang@aps.anl.gov>
2026-04-24 13:33:09 -0500

    explicitly convert to string

--- gestalt/convert/qt/QtShellCommand.py
+++ gestalt/convert/qt/QtShellCommand.py
@@ -3,64 +3,64 @@ from gestalt.Type import *
 from gestalt.convert.qt.QtWidget import QtWidget
 
 class QtShellCommand(QtWidget):
-	def __init__(self, node=None, macros={}):		
+	def __init__(self, node=None, macros={}):
 		super(QtShellCommand, self).__init__("QFrame", loc=node.location)
 
 		self.button = QtWidget("caShellCommand", node=node, macros=macros)
 		self["geometry"] = self.button.pop("geometry")
-		
+
 		self.button["label"] = String("-" + str(self.button.pop("text")))
-		
+
 		labels = ""
 		commands = ""
 		args = ""
-		
+
 		for item in node["commands"]:
 			labels += str(item.get("label", "")) + ";"
 			commands  += str(item.get("command", "")) + ";"
 			args   += ";"
-			
-			
+
+
 		self.button["labels"] = String(labels.removesuffix(";"))
 		self.button["files"]  = String(commands.removesuffix(";"))
 		self.button["args"]   = String(args.removesuffix(";"))
-		
+
 		self.button["fontScaleMode"] = Enum("EPushButton::WidthAndHeight")
-		
+
 		self.tocopy.append("button")
-		
-		
+
+
 	def updateProperties(self, macros={}):
 		super(QtShellCommand, self).updateProperties(macros)
 		self.button.updateProperties(macros)
-		
+
 	def setProperty(self, key, prop):
 		if key == "geometry":
 			super(QtShellCommand, self).setProperty(key, prop)
 		else:
 			self.button.setProperty(key, prop)
-			
+
 	def write(self, tree):
 		self.button["geometry"] = Rect((self["geometry"]["width"], self["geometry"]["height"]))
 		self.button.position(x=0,y=0)
-		
+
 		the_font = self.button.pop("font")
-		align = self.button.pop("alignment")
-		
+		align = str(self.button.pop("alignment"))
+
 		frame_align = "center"
-		
+
 		if "AlignLeft" in align:
 			frame_align = "left"
 		elif "AlignRight" in align:
 			frame_align = "right"
-		
+
 		self.properties["attrs"]["styleSheet"] = String(
 """\
 QPushButton
 {{
     font-family: {family};
     font: {style};
-	
+
     text-align: {lcr};
 }}
 """.format(
@@ -68,7 +68,7 @@ QPushButton
 	style  = the_font["style"].lower(),
 	size   = the_font["size"],
 	lcr    = frame_align))
-	
+
 		self.children.append(self.button)
-		
+
 		super(QtShellCommand, self).write(tree)
