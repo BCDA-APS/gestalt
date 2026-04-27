1e54603b7c9fd8d4380a5a915cc5a7537636b94a
keenanlang <klang@aps.anl.gov>
2026-04-27 11:39:30 -0500

    fix copy/paste issue

--- gestalt/nodes/StretchNode.py
+++ gestalt/nodes/StretchNode.py
@@ -1,7 +1,7 @@
 """
 A positioner used to stretch a widget to fill the space provided by its parent widget
 
-### AStrech
+### AStretch
 
 ---
 
@@ -10,7 +10,7 @@ Determines a widget's width and height to match up respectively with the widget'
 * **Example**
 
 ```yaml
-Fill_Parent: !astretch:Text            
+Fill_Parent: !astretch:Text
     text: "Middle"
     alignment: CenterLeft
 ```
@@ -29,7 +29,7 @@ Determines a widget's width to match up with the size of the widget's parent wid
 ```yaml
 UITitle: !hstretch:Text
     geometry: 0x32
-            
+
     text: "Middle"
     alignment: CenterLeft
 ```
@@ -40,7 +40,7 @@ UITitle: !hstretch:Text
 
 ---
 
-Determines a widget's width to match up with the size of the widget's parent width.
+Determines a widget's height to match up with the size of the widget's parent height.
 
 You may also use the alias "stretch" to reference the vstretch node.
 
@@ -50,7 +50,7 @@ You may also use the alias "stretch" to reference the vstretch node.
 ```yaml
 UITitle: !vstretch:Text
     geometry: 32x0
-            
+
     text: "Middle"
     alignment: CenterLeft
 ```
@@ -65,33 +65,33 @@ class StretchNode(Node):
 		if layout is None:
 			layout = {}
 		super(StretchNode, self).__init__("Stretch", name=name, layout=layout, loc=loc)
-				
+
 		self.setProperty("flow", flow, internal=True)
 		self.setProperty("render-order", 1)
-		
+
 		self.subnode = subnode
 		self.tocopy.append("subnode")
-		
-	
+
+
 	def apply (self, generator):
 		the_node = copy.deepcopy(self.subnode)
-		
+
 		if self.name:
 			the_node.name = self.name
-		
+
 		applier = the_node.apply(generator)
-		
+
 		for increment in applier:
 			data = yield
-			
+
 			flow = self["flow"].val()
-			
+
 			if flow == "vertical" or flow == "all":
 				the_node["geometry"]["height"] = data["__parentheight__"]
 			if flow == "horizontal" or flow=="all":
 				the_node["geometry"]["width"] = data["__parentwidth__"]
-			
+
 			applied_node = applier.send(data)
 			applied_node.placed_order = self.placed_order
-			
+
 			yield applied_node
